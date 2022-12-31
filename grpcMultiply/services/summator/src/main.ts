3c0ae4ddf2485f4aa68105310e32d2c28867ae3d
Swapnali <sonawanesn210@gmail.com>
2022-12-31 15:50:04 +0530

    added

---
+++ grpcMultiply/services/summator/src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import * as path from 'path';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const URL = 'localhost:50052';
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        url: URL,
+        package: 'summator',
+        protoPath: path.resolve(__dirname, '../../../api/summator.proto'),
+      },
+    },
+  );
+
+  await app.listen();
+  console.log('Summator service has started');
+}
+bootstrap();
